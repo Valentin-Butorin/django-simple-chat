9cb8a6eb971767187acd423b56fa9cfdb024167b
Valentin Butorin <arch1_cs@mail.ru>
2022-04-04 02:06:22 +0800

    added online counter

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,17 +1,47 @@
 import json
 from datetime import datetime
-
 from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.db import database_sync_to_async
+
+CLIENTS = set()
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
+
+    def get_secret_key(self):
+        for key, value in self.scope['headers']:
+            if key == b'sec-websocket-key':
+                return value.decode(encoding='utf-8')
+        return None
 
     async def connect(self):
         self.group_name = 'chat'
         await self.channel_layer.group_add(self.group_name, self.channel_name)
         await self.accept()
 
+        key = self.get_secret_key()
+        if key:
+            CLIENTS.add(key)
+
+            event = {
+                'type': 'send_message',
+                'users_count': len(CLIENTS)
+            }
+
+            await self.channel_layer.group_send(self.group_name, event)
+
     async def disconnect(self, code):
+        print(self.scope)
+        key = self.get_secret_key()
+        if key:
+            CLIENTS.remove(key)
+
+            event = {
+                'type': 'send_message',
+                'users_count': len(CLIENTS)
+            }
+            await self.channel_layer.group_send(self.group_name, event)
+
         await self.channel_layer.group_discard(self.group_name, self.channel_name)
 
     async def receive(self, text_data=None, bytes_data=None):
@@ -34,6 +64,14 @@ class ChatConsumer(AsyncWebsocketConsumer):
         await self.channel_layer.group_send(self.group_name, event)
 
     async def send_message(self, event):
-        message = event['message']
+        response = dict()
 
-        await self.send(text_data=json.dumps({'message': message}))
+        message = event.get('message')
+        if message:
+            response['message'] = message
+
+        users_count = event.get('users_count')
+        if users_count:
+            response['users_count'] = users_count
+
+        await self.send(text_data=json.dumps(response))
